846b1e033007d65245b10dd3f52bec043851e28a
harjot <harjot362.be22@chitkara.edu.in>
2025-06-25 15:29:17 +0530

    basic java add and division

---
+++ Day3/basic.java
@@ -0,0 +1,18 @@
+package Day3;
+
+public class basic {
+
+    public static void main(String[] args) {
+         int a = 10;
+        int b = 20;
+
+        int c = a + b;
+        System.out.println("Sum of a and b is " + c);
+
+        int d = a * b;
+        System.out.println("Product of a and b is " + d);
+
+        int e = a / b;
+        System.out.println("Division of a by b is " + e);
+    }
+}
